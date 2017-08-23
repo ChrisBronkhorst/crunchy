5afc6bf6a480238bf8011428fbc364abd442b8e8
Chris Bronkhorst <CBronkhorst@laingorourke.com.au>
2017-08-24 00:47:31 +1000

    add models to admin interface

--- crunch/admin.py
+++ crunch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Company, Sector, Investor, FundingRound, InvestorFundingRoundContribution
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Sector)
+admin.site.register(Investor)
+admin.site.register(FundingRound)
+admin.site.register(InvestorFundingRoundContribution)
