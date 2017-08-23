579dab3dbc8505e8b70efa6ce29f39eec725d548
Chris Bronkhorst <CBronkhorst@laingorourke.com.au>
2017-08-24 00:43:33 +1000

    add string representations to models

--- crunch/models.py
+++ crunch/models.py
@@ -9,13 +9,22 @@ class Company(models.Model):
     description = models.TextField()
     sector = models.ForeignKey(to='Sector', related_name='companies')
 
+    def __str__(self):
+        return self.name
+
 
 class Sector(models.Model):
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 
 class Investor(models.Model):
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 class FundingRound(models.Model):
@@ -23,6 +32,9 @@ class FundingRound(models.Model):
     company = models.ForeignKey(to=Company, related_name='funding_rounds')
     investors = models.ManyToManyField(to=Investor, through='InvestorFundingRoundContribution',
                                        related_name='funding_rounds')
+
+    def __str__(self):
+        return self.description
 
     @property
     def size(self):
@@ -33,6 +45,10 @@ class InvestorFundingRoundContribution(models.Model):
     investor = models.ForeignKey(to=Investor, related_name='investments')
     round = models.ForeignKey(to=FundingRound, related_name='contributions')
     amount = models.FloatField()
+
+    def __str__(self):
+        return "contribution of: %s" % self.amount
+
 
 
 
