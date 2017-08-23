4d51954c938060c779b2814a139b5efc095e67f8
Chris Bronkhorst <CBronkhorst@laingorourke.com.au>
2017-08-24 00:35:14 +1000

    add data models for companies, sectors, investors, funding rounds and contributors

--- crunch/models.py
+++ crunch/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+# 5 models, Person, Company, Investor, FundingRound, Sector, Employee
+
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    sector = models.ForeignKey(to=Sector, related_name='companies')
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Investor(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class FundingRound(models.Model):
+    description = models.TextField()
+    company = models.ForeignKey(to=Company, related_name='funding_rounds')
+    investors = models.ManyToManyField(to=Investor, through='InvestorFundingRoundContribution',
+                                       related_name='funding_rounds')
+
+
+class InvestorFundingRoundContribution(models.Model):
+    investor = models.ForeignKey(to=Investor, related_name='investments')
+    round = models.ForeignKey(to=FundingRound, related_name='contributions')
+    amount = models.FloatField()
+
+
+
+
+
